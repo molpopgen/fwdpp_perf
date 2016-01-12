44119bba5a9bf6fcff07324c604dbe8a6277e2f4
Kevin R. Thornton <krthornt@uci.edu>
2016-01-12 15:48:22 -0800

    write thread times to OFILE2

--- run.sh
+++ run.sh
@@ -9,8 +9,9 @@ RHO=$4
 for i in $(find . -maxdepth 1 -executable -type f)
 do
     OFILE=$i.$NTHREADS.N$N.t$THETA.r$RHO.time.txt
+    OFILE2=$i.$NTHREADS.N$N.t$THETA.r$RHO.thread_times.txt
     if [ ! -s $OFILE ]
     then
-	/usr/bin/time -f "%e %M" -o $OFILE ./$i $N $NTHREADS $THETA $RHO
+	/usr/bin/time -f "%e %M" -o $OFILE ./$i $N $NTHREADS $THETA $RHO > $OFILE2
     fi
 done
